5a7580ae0c7195a3ed5a1330b7a396704cbf0b9a
sungjin <byun970@gmail.com>
2024-11-21 13:06:01 +0900

    body-parser/post요청

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 
@@ -10,5 +18,19 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", async (req, res) => {
+  // 회원가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에서 가져온다
+
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.json({ success: false, err });
+  }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
