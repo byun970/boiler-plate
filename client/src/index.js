4f2af0b04457f66cd89298cf680f08166bbd5347
sungjin <byun970@gmail.com>
2024-11-26 15:30:10 +0900

    redux 설정

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "antd/dist/reset.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import { thunk } from "redux-thunk"; // Named import로 수정
+import Reducer from "./_reducers";
+
+// `createStoreWithMiddleware` 생성 및 redux-thunk와 promiseMiddleware 적용
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, thunk)(createStore);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f // `window.__REDUX_DEVTOOLS_EXTENSION__`이 없을 때 기본 값 제공
+    )}
+  >
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
